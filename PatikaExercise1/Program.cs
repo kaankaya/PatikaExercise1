fc4d71240a6eeb5acc2ecacd1adddc0c76d83c0c
Kaan Kaya <kaankaya.0707@gmail.com>
2024-07-16 02:12:27 +0300

    Add project files.

---
+++ PatikaExercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace patikaExercise
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Osman Kaan Kaya");
+            Console.WriteLine("Teknolojiye olan ilgim ve bu alanda kendimi geliştirerek kariyerimde ilerlemek istememdir; ayrıca, sektörün en iyi eğitimlerinden birini sunan Patika'nın bu konuda bana büyük katkı sağlayacağına inanıyorum.");
+        }
+    }
+}
